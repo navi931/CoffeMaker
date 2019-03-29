4f27f9efff7a0b0cd1b54bde70fa92a97ab8050f
MiguelRodriguezSayeg <41605911+MiguelRodriguezSayeg@users.noreply.github.com>
2019-03-28 22:42:08 -0600

    Add files via upload

--- source/HAL/LCD_Driver.c
+++ source/HAL/LCD_Driver.c
@@ -1,78 +1,102 @@
+#include "MKL27Z644.h"
 #include "LCD_Driver.h"
-void  delayUs(uint_16  n){
-	uint_8 i; uint_8 j;
-	for(i = 0 ; i < n; i++) {
-		for(j = 0; j < 8; j++) ;
+void  delayUs(uint_16  n)
+{
+	uint_8 i=0;
+	uint_8 j=0;
+	for(i = 0 ; i<n; i++)
+	{
+		for(j = 0; j<USINSTRUCTIONS; j++){
+
+		}
 	}
 }
 
-void LCD_init(void){
-SIM->SCGC5 |= 0x1000;
-PORTD->PCR[0]  =  0x100;
-PORTD->PCR[1]  =  0x100;
-PORTD->PCR[2]  =  0x100;
-PORTD->PCR[3]  =  0x100;
-PORTD->PCR[4]  =  0x100;
-PORTD->PCR[5]  =  0x100;
-PORTD->PCR[6]  =  0x100;
-PORTD->PCR[7]  =  0x100;
-GPIOD->PDDR  =  0xFF;
-SIM->SCGC5 |= 0x0200; /* habilita el reloj del puerto A */
-PORTA->PCR[2] = 0x100; /* usaremos el pin 2 del Puerto A */
-PORTA->PCR[4] = 0x100; /* usaremos el pin 4 del Puerto A */
-PORTA->PCR[5]  =  0x100;  /*  usaremos  el  pin  5  del Puerto A */
-GPIOA->PDDR  |=  0x34;  /*  hacemos  a  los  pines  2,  4  y  5 salidad */
-delayMs(30); /* secuencia de inicializacion del LCD */
-LCD_command(INITCOMMAND);
-delayMs(10);
-LCD_command(INITCOMMAND);
-delayMs(1);
-LCD_command(INITCOMMAND);
-LCD_command(TWOLINES); /* establece 8 lineas de datos al LCD */
-LCD_command(ENTRYMODESET); /* mueve el cursor a la derecha*/
-LCD_command(CLRSCR);  /*  limpia  la  pantalla  y  el  cursor regresa a la posicion de inicio */
-LCD_command(BLINK_CURSOR); /* el cursor parpadea */
+void LCD_init(void)
+{
+	SIM->SCGC5 |= 0x1000;			/*PORT D clock Initialization*/
+	PORTD->PCR[0]  =  0x100;		/*PORTS 0-7 are initialized*/
+	PORTD->PCR[1]  =  0x100;
+	PORTD->PCR[2]  =  0x100;
+	PORTD->PCR[3]  =  0x100;
+	PORTD->PCR[4]  =  0x100;
+	PORTD->PCR[5]  =  0x100;
+	PORTD->PCR[6]  =  0x100;
+	PORTD->PCR[7]  =  0x100;
+	GPIOD->PDDR  =  0xFF;			/*ALL PORTD PORTS ARE ENABLED AS OUTPUT*/
+	SIM->SCGC5 |= 0x0200; 			/* PORT A clock Initialization */
+	PORTA->PCR[2] = 0x100; 			/* Pin activation for PTA2,PTA4 and PTA5 */
+	PORTA->PCR[4] = 0x100;
+	PORTA->PCR[5]  =  0x100;
+	GPIOA->PDDR  |=  0x34; 			/*  PTA2,4 AND 5 are set as output */
+	delayMs(PAUSA1); 					/* LCD init sequence */
+	LCD_command(INITCOMMAND);
+	delayMs(PAUSA2);
+	LCD_command(INITCOMMAND);
+	delayMs(PAUSA3);
+	LCD_command(INITCOMMAND);
+	LCD_command(TWOLINES); 			/* two line mode */
+	LCD_command(ENTRYMODESET); 		/* Cursor will move to the right*/
+	LCD_command(CLRSCR);  			/*  Clear screen */
+	LCD_command(BLINK_CURSOR); 		/* Cursor blinks */
 }
-void LCD_command(uint_8 command) {
-GPIOA->PCOR = RS | RW;
-GPIOD->PDOR = command;
-GPIOA->PSOR = EN;
-delayMs(0);
-GPIOA->PCOR = EN;
-if (command < 4) delayMs(4);
-else delayMs(1);
-}
-
-void LCD_data(uint_8 data) {
-	GPIOA->PSOR = RS;
-	GPIOA->PCOR = RW;
-	GPIOD->PDOR = data;
-	GPIOA->PSOR = EN;
-	delayMs(1);
-	GPIOA->PCOR = EN;
-	delayMs(1);
+void LCD_command(uint_8 command)
+{
+	GPIOA->PCOR = RS | RW;
+	GPIOD->PDOR = command; /*El comando es enviado a través de los puertos D*/
+	GPIOA->PSOR = EN;	/*We activate the "enable" line*/
+	delayMs(LCDDATATIME);
+	GPIOA->PCOR = EN;	/*We deactivate the "enable" line*/
+	if (command < LCOMMANDSIZE){
+		delayMs(LCOMMANDSIZE);
+	}
+	else{
+		delayMs(SCOMMANDSIZE);
+	}
 }
 
-void delayMs(uint_16 n) {
-	int i; int j;
-	for(i = 0 ; i < n; i++)
-		for(j = 0 ; j < 7000; j++) { }
-
-
+void LCD_data(uint_8 data)
+{
+	GPIOA->PSOR = RS;	/*Read mode disabled */
+	GPIOA->PCOR = RW;	/*Write mode*/
+	GPIOD->PDOR = data; /*The command is sent through the connected PORTD ports*/
+	GPIOA->PSOR = EN; /*We activate the "enable" line*/
+	delayMs(LCDDATATIME);
+	GPIOA->PCOR = EN; /*We disable the "enable" line*/
+	delayMs(LCDDATATIME);
 }
-void sendString(uint_8 * array,uint_8 size){
-	uint_8 i;
-	LCD_command(CLRSCR);
-	delayMs(500);
-	LCD_command(DISPLAYBUFFER);
-	if(size<=32){
-		for(i=0;i<size;i++){
-			if(i==16){
-				LCD_command(JUMPLINE);
+
+void delayMs(uint_16 n)
+{
+	uint_8 i=0;
+	uint_8 j=0;
+	for(i = 0 ; i < n; i++){
+		for(j = 0 ; j < MSINSTRUCTIONS; j++){
+
+		}
+	}
+}
+void sendString(uint_8 * array,uint_8 size)
+{
+	uint_8 i=0;
+	LCD_command(CLRSCR); /*Clearing screen*/
+	delayMs(WAITCLEARTIME);
+	LCD_command(DISPLAYBUFFER); /*Print on the first line.*/
+	if(size<=(LCDLINES*2))
+	{
+		for(i=0;i<size;i++)
+		{
+			if(i!=LCDLINES)
+			{
+				LCD_data(*(array+i)); /*Inserting ASCII values to display.*/
 			}
-			else{
-				LCD_data(*(array+i));
+			else
+			{
+				LCD_command(JUMPLINE); /*Jumping to the next line*/
 			}
 		}
+	}
+	else{
+		/*SIZE OF THE STRING IS LARGER THAN 32 BYTES*/
 	}
 }
--- source/HAL/LCD_Driver.h
+++ source/HAL/LCD_Driver.h
@@ -11,8 +11,24 @@
 #define TWOLINES 0x38
 #define INITCOMMAND 0x30
 #define DISPLAYBUFFER 0x80
-#include "MKL27Z644.h"
-#include "tools.h"
+#define USINSTRUCTIONS 8
+#define MSINSTRUCTIONS 7000
+#define LCDLINES 16
+
+#define LCOMMANDSIZE 4
+#define SCOMMANDSIZE 1
+
+#define WAITCLEARTIME 500
+#define LCDDATATIME 1
+
+enum Init{
+	PAUSA1=30,
+	PAUSA2=10,
+	PAUSA3=1
+};
+
+typedef unsigned char uint_8;
+typedef unsigned long uint_16;
 void delayMs(uint_16 n);
 void LCD_command(uint_8 command);
 void LCD_data(uint_8 data);
